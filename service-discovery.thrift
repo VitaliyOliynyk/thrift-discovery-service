37d44ce4e7b5a59409a76a3448370727ee2afb9b
VitaliyOliynyk <xaoc00@gmail.com>
2014-07-26 15:36:41 +0200

    initial commit

---
+++ service-discovery.thrift
@@ -0,0 +1,12 @@
+struct ServiceInfo {
+   1: string host,
+   2: i16 port
+}
+
+exception ServiceNotFoundException {
+	1: string message
+}
+
+service DiscoveryService {
+	ServiceInfo getInfo(1:string serviceName) throws (1:ServiceNotFoundException serviceNotFound)	
+}
